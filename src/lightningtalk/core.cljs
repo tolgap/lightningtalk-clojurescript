b2aff818b5a10d50c757a9206a995e6573e45240
Tolga Paksoy <tolga@hoppinger.com>
2015-12-06 14:58:53 +0100

    Add a hello world index component

--- src/lightningtalk/core.cljs
+++ src/lightningtalk/core.cljs
@@ -1,22 +1,22 @@
 (ns lightningtalk.core
   (:require [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true]))
+            [sablono.core :as html :refer-macros [html]]))
 
 (enable-console-print!)
 
 (println "Edits to this text should show up in your developer console.")
 
-;; define your app data so that it doesn't get over-written on reload
+(defonce app-state (atom {:title "Hello world!"}))
 
-(defonce app-state (atom {:text "Hello world!"}))
+(defn index-component [data owner]
+  (om/component
+   (html [:div
+          (data :title)])))
 
 (om/root
-  (fn [data owner]
-    (reify om/IRender
-      (render [_]
-        (dom/h1 nil (:text data)))))
-  app-state
-  {:target (. js/document (getElementById "app"))})
+ index-component
+ app-state
+ {:target (. js/document (getElementById "app"))})
 
 
 (defn on-js-reload []
